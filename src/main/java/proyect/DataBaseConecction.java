ee50479aa25671265f64fd8c48e91b941f60ffe4
Gerardo Sanchez <114122029+xeras55@users.noreply.github.com>
2023-12-10 16:20:07 -0600

    a

--- src/main/java/proyect/DataBaseConecction.java
+++ src/main/java/proyect/DataBaseConecction.java
@@ -1,7 +1,9 @@
 package proyect;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,15 +11,29 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 public class DataBaseConecction {
+  @Autowired
+  public DataSource dataSource;
 
-  @Bean
-  public DataSource dataSource() {
+  public DataBaseConecction() {
     ComboPooledDataSource pooledDataSource = new ComboPooledDataSource();
-    pooledDataSource.setMaxPoolSize(20);
-    return pooledDataSource;
+    pooledDataSource.setMaxPoolSize(5);
+    this.dataSource = pooledDataSource;
   }
 
-  public DataSource getDataSource() {
-    return dataSource();
+  /*
+   * public DataSource dataSource() {
+   * ComboPooledDataSource pooledDataSource = new ComboPooledDataSource();
+   * pooledDataSource.setMaxPoolSize(5);
+   * return this.dataSource = pooledDataSource;
+   * }
+   */
+  @Bean
+  public Connection getConnection() {
+    try {
+      return this.dataSource.getConnection();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
+
 }
